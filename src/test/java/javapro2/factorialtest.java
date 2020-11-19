81ed1d0de3b7b44db52079bfdb4e29639b3f88e0
arjun <arjun123@gmail.com>
2020-11-19 14:26:24 +0530

    commit javap2

---
+++ src/test/java/javapro2/factorialtest.java
@@ -0,0 +1,14 @@
+package javapro2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class factorialtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
